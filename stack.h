917206fe81379a4df983e9902f5b75fd969bcc76
Tadeusz Tomoszek <Tadeuszjt@gmail.com>
2019-12-03 14:45:55 +0000

    Added do whiles back in

--- stack.h
+++ stack.h
@@ -5,21 +5,21 @@
 #define Stack(T) struct {T *array; int len, cap;}
 
 #define Stack_init(s)\
-{\
+do {\
 	(s)->len = 0;\
 	(s)->cap = 16;\
 	(s)->array = malloc(sizeof(*(s)->array) * 16);\
-}
+} while (0)
 
 #define Stack_push(s, item)\
-{\
+do {\
 	if ((s)->len >= (s)->cap) {\
 		(s)->cap = (s)->len * 2;\
 		*(void**)&(s)->array = realloc((s)->array,\
 			sizeof(*(s)->array) * (s)->cap);\
 	}\
 	(s)->array[(s)->len++] = (item);\
-}
+} while (0)
 
 #define Stack_pop(s, pItem)\
 ((s)->len <= 0 ? 0 : (\
@@ -31,6 +31,6 @@
 
 
 #define Stack_destroy(s)\
-{ free((s)->array); }
+do { free((s)->array); } while (0)
 
 #endif 
